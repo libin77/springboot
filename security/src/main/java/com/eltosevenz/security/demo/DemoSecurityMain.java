ddfeaec642d0940ba5b1f1af96c7afff04c5b104
libinlougine <slibinlougine@gmail.com>
2024-09-30 08:22:57 +0530

    initial commit -- security

---
+++ security/src/main/java/com/eltosevenz/security/demo/DemoSecurityMain.java
@@ -0,0 +1,13 @@
+package com.eltosevenz.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityMain.class, args);
+	}
+
+}
---
+++ security/src/test/java/com/eltosevenz/security/demo/DemoSecurityMainTests.java
@@ -0,0 +1,13 @@
+package com.eltosevenz.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
