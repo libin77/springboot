b0b5c8fdb2219118563fd1c001a40d11519b5259
libinlougine <slibinlougine@gmail.com>
2024-10-02 20:01:09 +0530

    transactional changes

--- transactional/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
--- Insert initial product data
-INSERT INTO PRODUCT_LIST (code, name, stock) VALUES ('P001', 'Laptop', 10);
-INSERT INTO PRODUCT_LIST (code, name, stock) VALUES ('P002', 'Smartphone', 20);
-INSERT INTO PRODUCT_LIST (code, name, stock) VALUES ('P003', 'Headphones', 50);
-
